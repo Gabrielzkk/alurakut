93934fc9c9d5fd8933096c84972b6f9f502391be
Gabriel Succi <zicanony@gmail.com>
2021-07-13 21:48:32 -0300

    Adicionando função para automatizar a extração da foto de perfil do GitHub.

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,27 @@ import styled from "styled-components";
 import MainGrid from '../src/componentes/MainGrid';
 import Box from "../src/componentes/Box";
 
+
+
+function ProfileSideBar(props) {
+  console.log(props);
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: "8px" }}/>
+    </Box>
+  )
+}
+
 export default function Home() {
 
   const githubUser = "Gabrielzkk";
-  
+
 
   return (
 
   <MainGrid>
     <div className="profileArea" style={{ gridArea: 'profileArea'}}>
-      <Box>
-        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: "8px" }}/>
-      </Box>
+      <ProfileSideBar githubUser={githubUser}/>
     </div>
 
     <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
